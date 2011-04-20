9e1fe77ca7046f4f56fa02ebbe476abb8077d57d
Sylvain Viollon <thefunny@gmail.com>
2011-04-20 16:32:41 +0200

    Setup test correctly.

---
+++ src/silva/app/document/testing.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2011 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+from Products.Silva.testing import SilvaLayer
+import silva.app.document
+import transaction
+
+
+class SilvaDocumentLayer(SilvaLayer):
+    default_packages = SilvaLayer.default_packages + [
+        'silva.app.document',
+        ]
+
+    def _install_application(self, app):
+        super(SilvaDocumentLayer, self)._install_application(app)
+        app.root.service_extensions.install('silva.app.document')
+        transaction.commit()
+
+FunctionalLayer = SilvaDocumentLayer(silva.app.document)
--- src/silva/app/document/tests/test_head.py
+++ src/silva/app/document/tests/test_head.py
@@ -5,33 +5,33 @@
 
 import unittest
 
-from silva.core.views.tests.test_head import HEADTestCase, PUBLIC_HEADERS_EXPECTED, AUTH, PRIVATE_HEADERS_EXPECTED
+from silva.app.document.testing import FunctionalLayer
+from silva.core.views.tests.test_head import HEADTestCase
+from silva.core.views.tests.test_head import (
+    PUBLIC_HEADERS_EXPECTED, AUTH, PRIVATE_HEADERS_EXPECTED)
 
 
 class DocumentHEADTestCase(HEADTestCase):
-    # XXX use infrae.testbrowser
+    layer = FunctionalLayer
 
     def test_document(self):
         factory = self.root.manage_addProduct['silva.app.document']
         factory.manage_addDocument('document', 'Document')
         self.check_headers(
-            '/root/document', {}, PUBLIC_HEADERS_EXPECTED)
-        self.assertEmptyResponse()
+            'HEAD', '/root/document', {}, PUBLIC_HEADERS_EXPECTED)
 
     def test_document_auth(self):
         factory = self.root.manage_addProduct['silva.app.document']
         factory.manage_addDocument('document', 'Document')
         self.check_headers(
-            '/root/document', AUTH, PUBLIC_HEADERS_EXPECTED)
-        self.assertEmptyResponse()
+            'HEAD',  '/root/document', AUTH, PUBLIC_HEADERS_EXPECTED)
 
     def test_document_auth_private(self):
-        factory = self.root.manage_addProduct['SilvaDocument']
+        factory = self.root.manage_addProduct['silva.app.document']
         factory.manage_addDocument('document', 'Document')
         self.set_private(self.root.document)
         self.check_headers(
-            '/root/document', AUTH, PRIVATE_HEADERS_EXPECTED)
-        self.assertEmptyResponse()
+            'HEAD', '/root/document', AUTH, PRIVATE_HEADERS_EXPECTED)
 
 
 
