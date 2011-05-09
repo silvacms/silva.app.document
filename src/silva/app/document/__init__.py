af3f5910179cf5c236c8412a0cfa82b766c6a249
Sylvain Viollon <thefunny@gmail.com>
2011-05-09 16:55:00 +0200

    Automatic extension installation.

--- src/silva/app/document/__init__.py
+++ src/silva/app/document/__init__.py
@@ -7,15 +7,17 @@ from Persistence import Persistent
 from five import grok
 from silva.core import conf as silvaconf
 from silva.core.conf.installer import DefaultInstaller
-from silva.core.interfaces import IContainerPolicy
 from silva.core.editor.interfaces import ICKEditorService
-from zope.interface import Interface
+from silva.core.interfaces import IContainerPolicy, IPublicationWorkflow
+from silva.core.interfaces.events import IInstalledExtensionEvent
 from zope.component import queryUtility
+from zope.interface import Interface
 
 
 silvaconf.extension_name("silva.app.document")
 silvaconf.extension_title("Silva new Document")
 silvaconf.extension_depends(["Silva", "silva.core.editor"])
+silvaconf.extension_default()
 
 
 class DocumentPolicy(Persistent):
@@ -41,6 +43,17 @@ class DocumentInstaller(DefaultInstaller):
         root.service_containerpolicy.register(
             'Silva new Document', DocumentPolicy, -1)
 
+
+@grok.subscribe(DocumentInstaller, IInstalledExtensionEvent)
+def add_default_document(extension, event):
+    root = event.root
+    if root.get_default() is None:
+        factory = root.manage_addProduct['silva.app.document']
+        factory.manage_addDocument('index', 'Welcome to Silva!')
+        document = root.index
+        version = document.get_editable()
+        version.body.save_raw_text('<div><h1>Welcome to Silva!</h1><p>Welcome to Silva! This is the public view. To actually see something interesting, try adding \'/edit\' to your url (if you\'re not already editing, you can <a href="edit">click this link</a>).</p>')
+        IPublicationWorkflow(document).publish()
 
 
 install = DocumentInstaller("silva.app.document", IExtension)
